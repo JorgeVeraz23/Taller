9b467960e0fee2be9cb07e06f458764a9d419830
JorgeVeraz23 <JorgeVeraZambrano@outlook.com>
2023-06-08 02:07:58 -0500

    first commit

---
+++ VeraZambrano/Models/Genero.cs
@@ -0,0 +1,9 @@
+﻿namespace VeraZambrano.Models
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Estado { get; set; }
+    }
+}
